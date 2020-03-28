21393b0d48108a1c510d1032122d48ebfa9ea6af
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2020-03-28 18:37:58 +0530

    Update MyMain.java

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,26 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj= new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(12);
+        obj.insert(8);
+        obj.insert(9);
+        obj.insert(17);
+        obj.insert(7);
+        obj.insert(19);
+        MyQueue obj1 = new MyQueue();
+        obj1.preOrderSucc(obj.root);
+        System.out.print("Pre-Order Successor of all the nodes are: ");
+        obj1.dispPreOrderSucc();
+    }
+
 }
