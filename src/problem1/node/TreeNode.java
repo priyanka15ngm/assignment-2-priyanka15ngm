29faa44fb202abfa6ddf977df679fc14f5336b60
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2020-03-28 18:23:26 +0530

    Update and rename MyMain.java to TreeNode.java

---
+++ src/problem1/node/TreeNode.java
@@ -0,0 +1,23 @@
+/*
+ *  Created by IntelliJ IDEA.
+ *  User: Vaibhav
+ *  Date: 23-Mar-20
+ *  Time: 7:17 PM
+ */
+package problem1.node;
+
+public class TreeNode {
+    private int data;
+    private TreeNode right;
+    private TreeNode left;
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
