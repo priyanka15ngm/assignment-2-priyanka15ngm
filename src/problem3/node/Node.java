61ee3e41351b7b7f1c5a049dcd946fd4f310981b
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2020-03-28 18:32:43 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,25 @@
  */
 package problem3.node;
 
+import problem5.student.Student;
+
 public class Node {
+    Student obj;
+    Node next;
+
+    public Student getObj() {
+        return obj;
+    }
+
+    public void setObj(Student obj) {
+        this.obj=obj;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
