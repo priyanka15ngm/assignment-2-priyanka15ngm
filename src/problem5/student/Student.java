d515191840fbff4f902c68d7984e49b1d98fdb93
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2020-03-28 18:49:50 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,39 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    String Name;
+    int roll;
+    int acount;
+    int backlog;
+    public Student(){};
+    public Student(int roll, int backlog, int acount) {
+        this.roll = roll;
+        this.acount = acount;
+        this.backlog = backlog;
+    }
+
+    public int getAcount() {
+        return acount;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
 }
