a43833f966390f124c4acc013dd105863c69a8db
priyanka nigam <45058302+priyanka15ngm@users.noreply.github.com>
2020-03-28 18:25:29 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,26 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String args[]) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(2);
+        obj.insert(7);
+        obj.insert(6);
+        obj.insert(4);
+        obj.insert(9);
+        obj.insert(11);
+        //obj.insert(8);
+        obj.preOrder(obj.root);
+        System.out.println();
+        obj.postOrder(obj.root);
+        System.out.println("\nFirst statement is true");
+        System.out.println("Second statement is false as we didn't get the middle point same.");
+    }
 }
